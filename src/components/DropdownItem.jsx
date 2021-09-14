5bf3236532a1197894bca81b7af537d60f014064
Christopher Woloshyn <cwolosh1@binghamton.edu>
2021-09-14 11:13:56 -0400

    Added homepage content and photos.

--- src/components/DropdownItem.jsx
+++ src/components/DropdownItem.jsx
@@ -1,6 +1,11 @@
 function DropdownItem(props) {
     return (
-        <a href={props.href} className="menu-button">
+        <a
+            href={props.href}
+            className="menu-button"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
             { props.leftIcon } { props.title }
         </a>
     );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,17 @@
+import Headshot from './../images/headshot-1.jpg'
+
+
 const Home = () => {
     return (
-        <h1>Hello World!</h1>
+        <div className="content-body">
+        <h1>Hi, I'm Christopher</h1>
+        <img src={Headshot} alt="Headshot of me!"></img>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut suscipit vitae dignissimos doloremque iusto nobis? Magni blanditiis a voluptas eius vero doloremque quam quasi quod excepturi, commodi odit sunt necessitatibus!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nemo nisi animi blanditiis et excepturi consequuntur quis laborum maiores, id totam quae consequatur ratione veniam? Voluptate consectetur a aliquid distinctio.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nemo nisi animi blanditiis et excepturi consequuntur quis laborum maiores, id totam quae consequatur ratione veniam? Voluptate consectetur a aliquid distinctio.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nemo nisi animi blanditiis et excepturi consequuntur quis laborum maiores, id totam quae consequatur ratione veniam? Voluptate consectetur a aliquid distinctio.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nemo nisi animi blanditiis et excepturi consequuntur quis laborum maiores, id totam quae consequatur ratione veniam? Voluptate consectetur a aliquid distinctio.</p>
+        </div>
     );
 }
  
