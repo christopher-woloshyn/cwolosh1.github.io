6299d2a0d0b931f7c8399c1a81983e2bd736cfae
Christopher Woloshyn <cwolosh1@binghamton.edu>
2021-09-28 16:39:50 -0400

    Updated content on Home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,21 +7,24 @@ const Home = () => {
         <div className="content-body">
             <h1>Hi, I'm Christopher</h1>
             <img src={Headshot} alt="Headshot of me!"></img>
-            <div className="paragraph-wrapper-with-button">
+            <div className="content-wrapper">
                 <p>
-                    I'm a Software Engineer with a strong background in Data
-                    Science and Mathematics. I have an applied skillset building
-                    end to end Machine Learning software using Python, Numpy,
-                    Pandas, and scikit-learn.
+                    I'm a Machine Learning Engineer with a strong background
+                    in Data Science and Mathematics. I have an applied skillset
+                    building end-to-end Machine Learning pipelines using Python,
+                    NumPy, Pandas, and scikit-learn. As a hobbiest I am
+                    passionate about graphic design and the principles of UI/UX 
+                    and have been learning JavaScript, CSS, and React.js to 
+                    turn my passion into action!
                 </p>
                 <div className="button-container">
                 <Link className="projects-button" to="/projects">View my Projects</Link>
                 </div>
                 <p>
                     I'm working hard to get my website fully operational.
-                    Many pages are incomplete, so for the time being, please
-                    enjoy this landing page, and keep checking back for more
-                    content coming soon!
+                    The interests page is still incomplete, so for the time
+                    being, please enjoy what I already have built out, and keep
+                    checking back for more content coming soon!
                 </p>
             </div>
         </div>
