af40ce87707eee66045c2abaf13e0eeac143b3a8
Christopher Woloshyn <cwolosh1@binghamton.edu>
2021-10-01 22:14:59 -0400

    Testing HashRouter instead of BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 // import Interests from './components/Interests';
